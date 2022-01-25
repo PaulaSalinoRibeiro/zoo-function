e237758c2c51bc60d183c2c3f9a5ab34556c1b2a
PAULA RIBEIRO <paulasalinor@gmail.com>
2022-01-25 16:50:41 -0300

    implementa funcao getEmployeeByName

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (employeeName === undefined) return {}; // default parameters
+  const { employees } = data;
+  return employees.find((e) => e.firstName === employeeName || e.lastName === employeeName);
 }
 
 module.exports = getEmployeeByName;
