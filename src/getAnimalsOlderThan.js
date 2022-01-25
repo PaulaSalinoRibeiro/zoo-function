6620fa1aa09d18fb911fa9b040d4013c5e567a7f
PAULA RIBEIRO <paulasalinor@gmail.com>
2022-01-25 16:23:43 -0300

    implementa funcao getAnimalsOlderThan

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const { species } = data;
+  const findAnimal = species.filter(({ name }) => name === animal);
+  return findAnimal[0].residents.every((e) => e.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
