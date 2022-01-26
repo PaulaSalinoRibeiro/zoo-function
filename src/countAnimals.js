5eb3129e4022ba23f0e67845ce99c0aeeaa614bb
PAULA RIBEIRO <paulasalinor@gmail.com>
2022-01-26 08:49:52 -0300

    implementa os dosi primeiros requisitos da fucao countAnimals

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,9 +1,20 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // without parameter --> map obj {specie: qta}
-  // with parameter --> find qta
-  // with parameter contain sex --> qta
-}
+const { species } = data;
 
+function countAnimals(animal) {
+  if (animal === undefined) {
+    return species.reduce((acc, { name, residents }) => {
+      acc[name] = residents.length;
+      return acc;
+    }, {});
+  }
+  if (Object.keys(animal).length === 1) {
+    const findAnimal = species.find(({ name }) => name === animal.specie);
+    return findAnimal.residents.length;
+  }
+}
+console.log(countAnimals({ specie: 'penguins' }));
+
+// with parameter contain sex --> qta
 module.exports = countAnimals;
