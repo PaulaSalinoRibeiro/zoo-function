6c4484f3552f6ee53353a246db58f83c73de3b8b
PAULA RIBEIRO <paulasalinor@gmail.com>
2022-01-27 11:53:29 -0300

    implementa funcao getEmployeesCoverage

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -3,22 +3,21 @@ const data = require('../data/zoo_data');
 const { species, employees } = data;
 
 const findEmployee = (obj) => {
-  const empl = Object.values(obj)[0];
-  const emp = employees.find((e) => (e.firstName === empl || e.lastName === empl || e.id === empl));
-  if (emp === undefined) throw new Error('Informações inválidas');
-  const idAnimals = emp.responsibleFor;
-  const animals = [];
-  const local = [];
-  idAnimals.forEach((idAnimal) => species.find((specie) => {
-    if (idAnimal === specie.id) {
-      animals.push(specie.name);
-      local.push(specie.location);
-    }
-  }));
-  return { id: emp.id,
-    fullName: `${emp.firstName} ${emp.lastName}`,
-    species: [...animals],
-    locations: [...local] };
+  const idAnimals = obj.responsibleFor;
+  const arrAnimals = [];
+  const arrLocal = [];
+  idAnimals.forEach((idAnimal) => {
+    species.forEach((specie) => {
+      if (idAnimal === specie.id) {
+        arrAnimals.push(specie.name);
+        arrLocal.push(specie.location);
+      }
+    });
+  });
+  return { id: obj.id,
+    fullName: `${obj.firstName} ${obj.lastName}`,
+    species: [...arrAnimals],
+    locations: [...arrLocal] };
 };
 
 function getEmployeesCoverage(obj) {
@@ -29,7 +28,10 @@ function getEmployeesCoverage(obj) {
     });
     return listEmployees;
   }
-  return findEmployee(obj);
+  const empl = Object.values(obj)[0];
+  const emp = employees.find((e) => (e.firstName === empl || e.lastName === empl || e.id === empl));
+  if (emp === undefined) throw new Error('Informações inválidas');
+  return findEmployee(emp);
 }
 
 module.exports = getEmployeesCoverage;
