4ee056e5c62a995e7fd876cef2fda6d0f17cf3c7
PAULA RIBEIRO <paulasalinor@gmail.com>
2022-01-31 23:13:54 -0300

    implementa funcao specieByRegion

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,27 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
+const regions = ['NE', 'NW', 'SE', 'SW'];
+
+const specieByRegion = () => {
+  const arrayAnimal = regions.reduce((acc, region) => {
+    const animals = species.filter((specie) => (specie.location === region));
+    acc[region] = animals.map((animal) => animal.name);
+    return acc;
+  }, {});
+  return arrayAnimal;
+};
+
+
 function getAnimalMap(options) {
-  //
+  // inputs
+  // includesName = true
+  // includesName = true && sort = true
+  // includesName = true && sex = female/macho
+  // includesName = true && sex = female/macho && sort = true
+
+  return specieByRegion();
 }
 
 module.exports = getAnimalMap;
