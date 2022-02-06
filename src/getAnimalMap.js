5047616dd86017dcff53de7ac9e037f01c34d538
PAULA RIBEIRO <paulasalinor@gmail.com>
2022-02-06 20:20:49 -0300

    requisito 7

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -2,28 +2,44 @@ const data = require('../data/zoo_data');
 
 const { species } = data;
 
-const regions = ['NE', 'NW', 'SE', 'SW'];
-
-const specieByRegion = () => {
-  const arrayAnimal = regions.reduce((acc, region) => {
-    const animals = species.filter((specie) => (specie.location === region));
-    acc[region] = animals.map((animal) => animal.name);
-    return acc;
-  }, {});
-  return arrayAnimal;
-};
-
-const animalNameByRegion = () => {
-  // return
-};
-
-function getAnimalMap(options) {
-  const { includesName, sex, sorted } = options;
-  if (includesName === true) {
-    return animalNameByRegion({ sex, sorted });
+const filterResidents = (residents, sex, sorted) => {
+  if (sex && sorted) {
+    return residents
+      .filter((resid) => resid.sex === sex)
+      .map(({ name }) => name)
+      .sort();
   }
+  if (sex) {
+    return residents
+      .filter((resid) => resid.sex === sex)
+      .map(({ name }) => name);
+  }
+  if (sorted) {
+    return residents.map(({ name: animalName }) => animalName).sort();
+  }
+  return residents.map(({ name: animalName }) => animalName);
+};
 
-  return specieByRegion();
+const animalMap = ({ includeNames, sex, sorted }) => {
+  const lc = { NE: [], NW: [], SE: [], SW: [] };
+  return species.reduce((acc, { name, location, residents }) => {
+    if (includeNames) {
+      acc[location].push({
+        [name]: filterResidents(residents, sex, sorted),
+      });
+    } else {
+      acc[location].push(name);
+    }
+    return acc;
+  }, lc);
+};
+
+function getAnimalMap(
+  options = { includeNames: false, sex: false, sorted: false },
+) {
+  if (options !== undefined) {
+    return animalMap(options);
+  }
 }
 
 module.exports = getAnimalMap;
